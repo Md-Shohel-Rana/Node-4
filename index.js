99aab5363454ae603c05e2404408077e31939852
Md Shohel Rana <113632658+Md-Shohel-Rana@users.noreply.github.com>
2023-05-19 21:53:49 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// const getRandomFruitsName = require('random-fruits-name')
+
+// console.log(getRandomFruitsName()) // Apple - English is default language
+
+var moviesNames = require('movies-names');
+
+console.log(moviesNames.random());
